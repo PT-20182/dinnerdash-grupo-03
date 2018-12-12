1755889be7486172165ecda844396c8a5674975a
Suniaster <thibousbr@gmail.com>
2018-12-12 08:42:58 -0200

    Finalizada merge migrations

--- db/migrate/20181205142316_add_meal_category_ref_to_meals.rb
+++
@@ -1,5 +0,0 @@
-class AddMealCategoryRefToMeals < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :meals, :meal_categories, foreign_key: true
-  end
-end
--- db/migrate/20181208195937_add_meal_to_meal_category.rb
+++
@@ -1,5 +0,0 @@
-class AddMealToMealCategory < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :meal_categories, :meal, foreign_key: true
-  end
-end

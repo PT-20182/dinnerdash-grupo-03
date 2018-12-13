9d57f9f5c9571239298d936a5221570ac5ef26cd
Antonio Henrique <ahmoura1995@gmail.com>
2018-12-13 13:29:00 -0200

    corrigindo forms

--- app/controllers/control_users_controller.rb
+++ app/controllers/control_users_controller.rb
@@ -1,5 +1,14 @@
 class ControlUsersController < ApplicationController
+  before_action :check_user
   def index
     @users = User.all
   end
+  private
+
+  def check_user
+    unless user_signed_in? && current_user.admin
+      redirect_to :root
+    end
+  end
+
 end
